58b61b45937459a3a5f3cba709094839bde142ee
Rodousse <rdousse63@gmail.com>
2016-11-16 16:48:48 +0100

    New Repo

---
+++ Assets/Script/BlurEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlurEffect : MonoBehaviour {
+
+    public Material shaderEffect;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        //mat is the material containing your shader
+        Graphics.Blit(source, destination, shaderEffect);
+    }
+}
